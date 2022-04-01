b9b4ab51f278bd8d832e141eb952153f14626399
francijpg <hellofrancijpg@gmail.com>
2022-04-01 00:10:02 -0300

    Create blog-template.js

---
+++ src/templates/blog-template.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/Layout"
+import ReactMarkdown from "react-markdown"
+
+const ComponentName = ({ data }) => {
+  const { content } = data.blog
+  console.log(data)
+  return (
+    <Layout>
+      <section className="blog-template">
+        <div className="section-center">
+          <article>
+            <ReactMarkdown children={content} />
+          </article>
+          <Link to="/blog" className="btn center-btn">
+            blog
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query GetSingleBlog($slug: String) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      content
+    }
+  }
+`
+
+export default ComponentName
